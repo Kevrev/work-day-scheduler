34d27a95004a04b70eeab2f106f94139efa4a48e
Seer <kmkevins97@gmail.com>
2023-03-13 00:09:16 -0700

    Working on save all button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
+// Only runs when ready
 $(document).ready(function() {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  
+  // Date and time
+  $('#currentDay').text(dayjs().format('h:mmA, dddd, MMMM D, YYYY'));
 
   // Puts localStorage data into its respective boxes upon page load
    function loadLocalStorage() {
@@ -17,7 +14,8 @@ $(document).ready(function() {
     }
   
   loadLocalStorage()
-    
+  
+  // Saves Data on click
   $('.saveBtn').on('click', function() { 
     var hour = $(this).closest('section').attr('id');
     var memo = $(this).siblings('.description').val();
@@ -28,15 +26,18 @@ $(document).ready(function() {
     $('.notification').fadeOut(2300);
   });
 
-  // Testing save all button
-  // $('.saveAllBtn').on('click', function () { 
-  //   var hour = $(this).closest('div').attr('id');
-  //   var memo = $(this).siblings('.description').val();
+  // $('.allBtn').on('click', function() {
+  //   $('.time-block').each(function() {
+  //     var hour = $(this).closest('section').attr('id');
+  //     var memo = $(this).siblings('.description').val();
+  
+  //     if (memo !== undefined) {
+  //       localStorage.setItem(hour, memo);
+  //     }
 
-  //   localStorage.setItem(hour, memo);
-
-  //   $('.notification').css("display", "block");
-  //   $('.notification').fadeOut(2300);
+  //     $('.notification').css("display", "block");
+  //     $('.notification').fadeOut(2300);
+  //   });
   // });
 
   function blockUpdater() { 
@@ -75,14 +76,11 @@ $(document).ready(function() {
   // updates every 5 seconds
   }, 5000); 
 
-  // Todo: add clear button
+  // Clears local storage on click, perhaps add confirmation modal
   $('#clear-all').on('click', function() {
     localStorage.clear();
     $('.description').val('')
   });
-
-  // Date and time
-  $('#currentDay').text(dayjs().format('h:mmA, dddd, MMMM D, YYYY'));
 
 });
 
